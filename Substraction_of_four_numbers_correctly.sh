fe96843ae115df0400207ff72e743db89c1ccad2
root <root@LAPTOP-33N12T6F.>
2025-01-27 18:35:49 -0500

    Result calculated sucessfuly

---
+++ Substraction_of_four_numbers_correctly.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#calculate these numbers for the resuly
+RESULT=$(./Substraction_of_four_numbers.sh 100 50 10 10)
+
+echo "result can be calculated: $Result"
+
+#statements
+if [[ $RESULT -eq 30 ]]; then
+   echo "result is 30"
+else
+   echo "error"
+fi
